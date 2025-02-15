1514935c3451ff517b82ec469ba2648819291c85
yuchan <yuchan8003@naver.com>
2025-02-15 21:07:41 +0900

    Fix: 그룹 삭제 수정 2트

--- src/repositories/postRepository.js
+++ src/repositories/postRepository.js
@@ -148,38 +148,15 @@ async function updatePost(post) {
 	};
 }
 
-
-
-
-
-// 그룹 삭제 함수
-async function deleteGroupById(group) {
-	return await prisma.$transaction(async (prisma) => {
-		const posts = await prisma.post.findMany({
-			where: { groupId: group.id },
-			select: { id: true },
-		});
-
-		const postIds = posts.map(post => post.id);
-
-		if (postIds.length > 0) {
-			await prisma.comment.deleteMany({
-				where: { postId: { in: postIds } },
-			});
-
-			await prisma.postTag.deleteMany({
-				where: { postId: { in: postIds } },
-			});
-
-			await prisma.post.deleteMany({
-				where: { id: { in: postIds } },
-			});
-		}
-
-		await prisma.group.delete({
-			where: { id: group.id },
-		});
+// 게시글 삭제를 위한 함수
+async function deletePostById(postId) {
+	const deletedPost = await prisma.post.delete({
+		where: {
+			id: postId,
+		},
 	});
+
+	return deletedPost;
 }
 
 
